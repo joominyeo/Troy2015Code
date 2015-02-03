bcbb67d04209fc83bb368fbc03a86aafda55b906
iNocturnis <inocturnis@gmail.com>
2015-02-03 13:24:01 -0800

    Adding Testing Contour Into the class ImageProcess *Note camera is never close still*

--- src/org/usfirst/frc/team3952/robot/ImageProcess.java
+++ src/org/usfirst/frc/team3952/robot/ImageProcess.java
@@ -8,9 +8,13 @@ import java.net.ServerSocket;
 
 
 
+
+
 import com.ni.vision.NIVision;
 import com.ni.vision.NIVision.DrawMode;
 import com.ni.vision.NIVision.Image;
+import com.ni.vision.NIVision.Point;
+import com.ni.vision.NIVision.ROI;
 import com.ni.vision.NIVision.ShapeMode;
 
 import edu.wpi.first.wpilibj.CameraServer;
@@ -20,20 +24,22 @@ import edu.wpi.first.wpilibj.Timer;
 public class ImageProcess {
 	Image frame;
 	int session;
+	ROI cam;
+	Point start;
+	Point stop;
 	public ImageProcess(){
 		frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
 		session = NIVision.IMAQdxOpenCamera("cam0",
 	    NIVision.IMAQdxCameraControlMode.CameraControlModeController);
 	    NIVision.IMAQdxConfigureGrab(session);
-
+	    start = new Point(0,0);
+	    stop = new Point(640,640);
 	}
 	public void startCamera(){
         NIVision.IMAQdxStartAcquisition(session);
         NIVision.Rect rect = new NIVision.Rect(10, 10, 100, 100);
-        
         NIVision.IMAQdxGrab(session, frame, 1);
-        NIVision.imaqDrawShapeOnImage(frame, frame, rect,
-        		DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
+	    NIVision.imaqAddLineContour(cam, start, stop);
         CameraServer.getInstance().setImage(frame);
         Timer.delay(0.005);		// wait for a motor update time
         
